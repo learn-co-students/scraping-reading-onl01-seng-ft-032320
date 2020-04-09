d229e467fa524fbabbec142b4819a554d39f99ab
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-09 20:39:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("https://flatironschool.com/") #stores html in a temporary file
+
+#HTML is a method of the gem Nokogiri and converts the large string into a bunch of nested nodes
+doc = Nokogiri::HTML(html)
+
+
+doc.css(".headline-26OIBN").text
+puts doc.css(".headline-26OIBN").text
